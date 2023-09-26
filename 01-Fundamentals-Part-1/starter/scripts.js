4ecdbe92a8306ec8c70459c09562d0b8d63cf5a5
dp95000 <davepollan@gmail.com>
2023-09-26 00:14:37 -0400

    section 2 Sept 26.23

--- 01-Fundamentals-Part-1/starter/scripts.js
+++ 01-Fundamentals-Part-1/starter/scripts.js
@@ -1,7 +1,60 @@
-let js = 'amazing';
-if (js === 'amazing') alert('JavaScript is FUN!');
-console.log(40 + 8 + 23 - 10);
+/* 
+let country = "USA";
+let continent = "N. America";
+let population = "350 Million";
 
-console.log('Jonas');
+console.log(country);
+console.log(continent);
+console.log(population);
+
+// line break
+let myFirstJob = 'Auto Tech';
+let myCurrentJob = 'Programmer';
+
+console.log(myFirstJob);
+console.log(myCurrentJob);
+
+let kids;
+console.log(kids);
+
 let quote = "My name is David and I will succeed"
 console.log(quote);
+
+*/
+
+/* 
+let JavascriptIsFun = true;
+console.log(JavascriptIsFun);
+console.log(typeof JavascriptIsFun);
+
+// let affirmation = 'you are awesome!'
+// console.log(affirmation);
+
+// console.log(typeof true);
+// console.log(typeof 11);
+// console.log(typeof affirmation);
+
+// NOTE: you only need to use the 'let" when declaring the value of a variable the first time. When you call it again to 
+// change it's value, you don't need to use 'let' again.
+JavascriptIsFun = 'Yes!';
+console.log(JavascriptIsFun);
+console.log(typeof JavascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 2002;
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null);
+
+*/
+
+let age = 30;
+age = 40;
+// const is used for variables whos values don't change.  Once a const vairable is assigned a value, it can't be reassigned later
+// and trying to do so will trigger an error message in console.  Const variables also can't be empty.
+const birthYear = 1983;
+birthYear = 1997;
