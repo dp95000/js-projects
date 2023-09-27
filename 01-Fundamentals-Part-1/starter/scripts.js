e105c4e7f425958272fb5924574bb3614d3ebffd
dp95000 <davepollan@gmail.com>
2023-09-27 00:06:47 -0400

    sept 27.2023 section 2.13

--- 01-Fundamentals-Part-1/starter/scripts.js
+++ 01-Fundamentals-Part-1/starter/scripts.js
@@ -52,9 +52,42 @@ console.log(typeof null);
 
 */
 
+/*
 let age = 30;
 age = 40;
 // const is used for variables whos values don't change.  Once a const vairable is assigned a value, it can't be reassigned later
 // and trying to do so will trigger an error message in console.  Const variables also can't be empty.
 const birthYear = 1983;
 birthYear = 1997;
+*/
+
+// 9-26-23
+const currentYear = 2037
+const ageJonas = currentYear - 1991;
+const ageSarah = currentYear - 2018;
+console.log(ageJonas, ageSarah);
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 which = 2*2*2
+
+const firstName = 'Jonas';
+const lastName = "Pollan";
+console.log(firstName + lastName);
+console.log(firstName + ' ' + lastName);
+
+// Assignment operators
+let x = 10 + 5; // answer is 15
+console.log(x);
+x += 10; // this means x = x + 10 (answer is 25)
+console.log(x);
+x *= 4; // this means x = x * 4 (answer should be 100)
+console.log(x);
+x++; // this means x = x + 1 (answer should be 101)
+console.log(x);
+x-- // this means x = x - 1 (answer hear should 100)
+console.log(x);
+
+// comparison operators
+console.log(ageJonas > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 18);
+const isFullAge = ageSarah >= 18;
+console.log(currentYear - 1991 > currentYear - 2018);
